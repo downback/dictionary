3f157342238199e9338051eb9e6e274d06f426fb
downback <daeun.down.back@gmail.com>
2023-11-06 23:17:01 +0100

    ADD detail pages

--- script.js
+++ script.js
@@ -8,29 +8,34 @@ const itVideo = document.querySelector(".gender");
 fingerBtn.addEventListener("mouseenter", function () {
     fingerVideo.classList.add("visible");
     fingerVideo.play();
+    vorVideo.classList.remove("visible");
+    vorVideo.pause();
+    itVideo.classList.remove("visible");
+    itVideo.pause();
 });
 
-fingerBtn.addEventListener("mouseleave", function () {
-    fingerVideo.classList.remove("visible");
-    fingerVideo.pause();
-});
 
 vorBtn.addEventListener("mouseenter", function () {
     vorVideo.classList.add("visible");
     vorVideo.play();
+    fingerVideo.classList.remove("visible");
+    fingerVideo.pause();
+    itVideo.classList.remove("visible");
+    itVideo.pause();
 });
 
-vorBtn.addEventListener("mouseleave", function () {
-    vorVideo.classList.remove("visible");
-    vorVideo.pause();
-});
 
 itBtn.addEventListener("mouseenter", function () {
     itVideo.classList.add("visible");
     itVideo.play();
+    fingerVideo.classList.remove("visible");
+    fingerVideo.pause();
+    vorVideo.classList.remove("visible");
+    vorVideo.pause();
 });
 
-itBtn.addEventListener("mouseleave", function () {
-    itVideo.classList.remove("visible");
-    itVideo.pause();
-});
+
+//itBtn.addEventListener("mouseleave", function () {
+//    itVideo.classList.remove("visible");
+//    itVideo.pause();
+//});
