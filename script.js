b35d1926660da24421d5818d39112a604c6a3388
downback <daeun.down.back@gmail.com>
2023-12-07 01:57:10 +0100

    CHNAGE classname

--- script.js
+++ script.js
@@ -1,56 +1,62 @@
-const fingerBtn = document.querySelector(".finger");
-const fingerVideo = document.querySelector(".fussfinger");
-const vorBtn = document.querySelector(".vor");
-const vorVideo = document.querySelector(".vorne");
-const itBtn = document.querySelector(".it");
-const itVideo = document.querySelector(".gender");
-const fleischBtn = document.querySelector(".fleisch");
-const fleischVideo = document.querySelector(".fruchtfleisch");
+const rBtn = document.querySelector(".1btn");
+const rVideo = document.querySelector("#r");
+const gwiBtn = document.querySelector(".2btn");
+const gwiVideo = document.querySelector("#gwi");
+const bBtn = document.querySelector(".3btn");
+const bVideo = document.querySelector("#b");
+const gmBtn = document.querySelector(".4btn");
+const gmVideo = document.querySelector("#gm");
+const fBtn = document.querySelector(".5btn");
+const fVideo = document.querySelector("#f");
+const vBtn = document.querySelector(".6btn");
+const vVideo = document.querySelector("#v");
+const yBtn = document.querySelector(".7btn");
+const yVideo = document.querySelector("#y");
+const dBtn = document.querySelector(".8btn");
+const dVideo = document.querySelector("#d");
+const sBtn = document.querySelector(".9btn");
+const sVideo = document.querySelector("#s");
+const aBtn = document.querySelector(".10btn");
+const aVideo = document.querySelector("#a");
+const ysBtn = document.querySelector(".11btn");
+const ysText = document.querySelector("#ys");
 
-fingerBtn.addEventListener("mouseenter", function () {
-    fingerVideo.classList.add("visible");
-    fingerVideo.play();
-    vorVideo.classList.remove("visible");
-    vorVideo.pause();
-    itVideo.classList.remove("visible");
-    itVideo.pause();
-    fleischVideo.classList.remove("visible");
-    fleischVideo.pause();
+rBtn.addEventListener("mouseenter", function () {
+    rVideo.classList.add("visible");
+    rVideo.play();
+    gwiVideo.classList.remove("visible");
+    gwiVideo.pause();
+    bVideo.classList.remove("visible");
+    bVideo.pause();
+    gmVideo.classList.remove("visible");
+    gmVideo.pause();
+    fVideo.classList.remove("visible");
+    fVideo.pause();
+    vVideo.classList.remove("visible");
+    vVideo.pause();
+    yVideo.classList.remove("visible");
+    yVideo.pause();
+    dVideo.classList.remove("visible");
+    dVideo.pause();
+    sVideo.classList.remove("visible");
+    sVideo.pause();
+    aVideo.classList.remove("visible");
+    aVideo.pause();
+    ysText.classList.remove("visible");
 });
 
 
-vorBtn.addEventListener("mouseenter", function () {
-    vorVideo.classList.add("visible");
-    vorVideo.play();
-    fingerVideo.classList.remove("visible");
-    fingerVideo.pause();
-    itVideo.classList.remove("visible");
-    itVideo.pause();
-    fleischVideo.classList.remove("visible");
-    fleischVideo.pause();
+gwiBtn.addEventListener("mouseenter", function () {
+
 });
 
 
-itBtn.addEventListener("mouseenter", function () {
-    itVideo.classList.add("visible");
-    itVideo.play();
-    fingerVideo.classList.remove("visible");
-    fingerVideo.pause();
-    vorVideo.classList.remove("visible");
-    vorVideo.pause();
-    fleischVideo.classList.remove("visible");
-    fleischVideo.pause();
+bBtn.addEventListener("mouseenter", function () {
+
 });
 
-fleischBtn.addEventListener("mouseenter", function () {
-    fleischVideo.classList.add("visible");
-    fleischVideo.play();
-    fingerVideo.classList.remove("visible");
-    fingerVideo.pause();
-    vorVideo.classList.remove("visible");
-    vorVideo.pause();
-    itVideo.classList.remove("visible");
-    itVideo.pause();
+gmBtn.addEventListener("mouseenter", function () {
+
 });
 
 
